e01e1529c501d3f7c715c42a57f8f2fd2315c860
Hector Alfaro <hectorsector@github.com>
2019-02-10 14:51:31 -0800

    add a route when environment variables aren't set

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index', {sha: process.env.GITHUB_SHA, name: process.env.GITHUB_ACTOR})
+  if (process.env.GITHUB_ACTOR && process.env.GITHUB_SHA) {
+    res.render('index', {sha: process.env.GITHUB_SHA, name: process.env.GITHUB_ACTOR})
+  } else {
+    res.render('almost')
+  }
 })
 
 app.listen(3000)
