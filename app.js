385237a7a673a6db423cff8d308589cb55400479
Hector Alfaro <hectorsector@github.com>
2019-02-12 18:39:48 -0500

    use a 6-digit sha for prettier

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
   if (process.env.GITHUB_ACTOR && process.env.GITHUB_SHA) {
-    res.render('index', {sha: process.env.GITHUB_SHA, name: process.env.GITHUB_ACTOR})
+    res.render('index', {sha: process.env.GITHUB_SHA.substring(process.env.GITHUB_SHA, 6), name: process.env.GITHUB_ACTOR})
   } else {
     res.render('almost')
   }
